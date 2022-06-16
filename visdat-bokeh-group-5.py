19e0d7377b2b6647e52e183a8d9fb8f8cd5be51b
Muhammad Attalariq <57281549+attalariq123@users.noreply.github.com>
2022-06-16 21:34:00 +0700

    Update visdat-bokeh-group-5.py

--- visdat-bokeh-group-5.py
+++ visdat-bokeh-group-5.py
@@ -20,8 +20,6 @@ Anggota:
 
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 from bokeh.io import output_file, output_notebook,curdoc
 from bokeh.plotting import figure, show
